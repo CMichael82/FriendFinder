7d6994f1519bf9424eb7188e9c687bf65caa5056
CAM <34009133+CMichael82@users.noreply.github.com>
2018-10-25 22:27:51 -0600

    started comparison logic

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -1,12 +1,23 @@
 var friends = require("../app/data/friends");
 
-module.exports = function (app){
-	app.get("/api/friends", function(req, res){
+module.exports = function (app) {
+	app.get("/api/friends", function (req, res) {
 		res.json(friends);
 	});
 
-	app.post("/api/friends", function(req, res){
+	app.post("/api/friends", function (req, res) {
 		friends.push(req.body);
 		res.json(true);
+		var currentScores = req.body.scores;
+		var difference;
+		console.log(currentScores);
+		for (var i = 0; i < friends.length; i++) {
+			
+			console.log(friends[i].scores);
+			// for (var j = 0; j<friends[i].scores.length; j++){
+			// 	console.log(friends.scores[j]);
+			// }
+			
+		}
 	});
 };
