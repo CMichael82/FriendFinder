d1c93e38c621cc43639b77f467663b8b397e25e7
CAM <34009133+CMichael82@users.noreply.github.com>
2018-10-25 21:21:07 -0600

    basic functionality without comparison logic

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ var PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+require("./routing/apiRoutes")(app);
+require("./routing/htmlRoutes")(app);
+
 app.listen(PORT, function(){
 	console.log("You are listening on " + PORT);
 });
